3fd5d8f6d5978fc3db87cf1e2a5b9cb89f5006e5
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2011-03-29 13:28:40 +0000

    added reading paths from INI

--- Fb2ePubGui/MainForm.cs
+++ Fb2ePubGui/MainForm.cs
@@ -177,13 +177,53 @@ namespace Fb2ePubGui
                 }
             }
             comboBoxDestination.Items.Add(defaultPath);
+            string iniPath = DetectIniLocation();
+            if (iniPath != string.Empty)
+            {
+                string count = IniReadValue(iniPath, "TARGETS", "TargetsCount");
+                int targetsCount;
+                if (int.TryParse(count,out targetsCount) && targetsCount > 0)
+                {
+                    for (int i = 0; i < targetsCount; i++)
+                    {
+                        string section = string.Format("Target{0}",i+1);
+                        string path = IniReadValue(iniPath, section, "TargetPath");
+                        if (!string.IsNullOrEmpty(path) && Directory.Exists(path))
+                        {
+                            comboBoxDestination.Items.Add(path);
+                        }
+                    }
+                }
+            }
+        }
 
+        private string DetectIniLocation()
+        {
+            string path2Exe =  Assembly.GetEntryAssembly().Location;
+            string folderPath = Path.GetDirectoryName(path2Exe);
+            if (folderPath == null)
+            {
+                folderPath = string.Empty;
+            }
+            string path2INI = Path.Combine(folderPath, "FB2EPUBExt.INI");
+            if (File.Exists(path2INI))
+            {
+                return path2INI;
+            }
 
+            return string.Empty;
         }
 
         private void buttonShowFolder_Click(object sender, EventArgs e)
         {
             Process.Start(comboBoxDestination.Text);
+        }
+
+        public string IniReadValue(string iniPath,string Section, string Key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(Section, Key, "", temp, 255, iniPath);
+            return temp.ToString();
         }
     }
 }

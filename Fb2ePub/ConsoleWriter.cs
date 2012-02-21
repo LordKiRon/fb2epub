4d7bfb3dc5ccf2d38b772cd04562bbf4212ed0a9
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2012-02-21 12:01:37 +0000

    Console output now shows more than one page of help

---
+++ Fb2ePub/ConsoleWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fb2ePub
+{
+    /// <summary>
+    /// Used to output text to console in such way it shown with pauses even if it does not fit the screen
+    /// </summary>
+    internal class ConsoleWriter
+    {
+        private readonly int _consoleHeight = Console.WindowHeight;
+
+        public void WriteLine(string stringToWrite)
+        {
+            int cursorPosition = Console.CursorTop;
+            if (cursorPosition + 2 >= _consoleHeight)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            Console.WriteLine(stringToWrite);
+        }
+        public void WriteLine()
+        {
+            WriteLine("");
+        }
+    }
+}

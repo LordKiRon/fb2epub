0d2c2f402adbb21c7a022253c2be4173cc9d133f
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2014-06-05 06:44:46 +0000

    Updated code with dynamic creation of FB2ePub menu entry in case FB2 file selected

--- FFPlugin/Fb2ePubPlugin/chrome/content/fb2-unknownContentType.js
+++ FFPlugin/Fb2ePubPlugin/chrome/content/fb2-unknownContentType.js
@@ -33,19 +33,59 @@ isfb2extension: function(sourceURI)
  return false;
 },
 
+insertMenu: function (saveGroup)
+{
+  var saveItem = document.getElementById('save');
+  if ( saveItem == null)
+  {
+    dump("\nItem with ID save not found");
+  }
+  else
+  {
+    var saveIndex = saveGroup.getIndexOfItem(saveItem);
+    var newMenuItem = null;
+	if (saveIndex  == saveGroup.itemCount ) // if 'save' - last item we append
+	{
+	  newMenuItem = saveGroup.appendItem("Fb2ePub (convert to ePub)");
+	}
+	else // else insert before it
+	{
+	  newMenuItem = saveGroup.insertItemAt(saveIndex+1,"Fb2ePub (convert to ePub)");
+	}
+	if ( newMenuItem == null )
+	{
+		dump("\nProblem");
+	}
+	else
+	{
+		newMenuItem.id = "fb2epub-radio";
+	}
+  }
+},
 
 init: function()
 {
 dump("\n");
 	if ( this.isfb2extension(dialog.mLauncher.source))
 	{
-		dump("\nFB2 file downloading");
+		//dump("\nFB2 file downloading");
+		var saveGroup = document.getElementById('mode');
+		if ( saveGroup == null )
+		{
+		  dump ("\nMode radiogroup not found");
+		}
+		else
+		{
+			this.insertMenu(saveGroup);
+		}
 	}
 	else
 	{
-		dump("\nNot FB2 file downloading");
+		//dump("\nNot FB2 file downloading");
 	}
 },
+
+
 
 onload: function()
 {

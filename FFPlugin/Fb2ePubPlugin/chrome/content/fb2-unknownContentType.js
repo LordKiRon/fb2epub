e6fc5ffbd9839ec8375b3935b0d2b3a4acc3f267
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2014-06-06 14:10:20 +0000

    updated with enable/diusable code (unfinished)

--- FFPlugin/Fb2ePubPlugin/chrome/content/fb2-unknownContentType.js
+++ FFPlugin/Fb2ePubPlugin/chrome/content/fb2-unknownContentType.js
@@ -4,6 +4,11 @@ window.addEventListener("load", function load(event){
 },false);
 
 var fb2SaveContent = {
+	_original_onaccept_action: null, // used to store original "onaccept" action
+	_fb2MenuItem: null, // menu item we insert
+	_fb2ItemSelected: false,
+	_rememberButtonDisabledPrevState: false,
+	_rememberButtonCheckedPrevState: false,
 
 // Check if the passed source name is one of FB2 extensions
 isfb2extension: function(sourceURI)
@@ -33,42 +38,150 @@ isfb2extension: function(sourceURI)
  return false;
 },
 
+// set the disabled state for remember button
+disableRememberButton: function(disable)
+{
+	var rememberbutton = document.getElementById('rememberChoice');
+	if ( rememberbutton == null )
+	{
+	  dump("\ndisableRememberButton: - rememberChoice button not found");
+	  return;
+	}
+	rememberbutton.setAttribute("disabled", disable);
+},
+
+// set the checked state for remember button
+checkRememberButton: function(check)
+{
+	var rememberbutton = document.getElementById('rememberChoice');
+	if ( rememberbutton == null )
+	{
+	  dump("\ncheckRememberButton: - rememberChoice button not found");
+	  return;
+	}
+	dump("\nSetting: " +check);
+	rememberbutton.setAttribute("checked", check);
+},
+
+// react on user clicking on one of the radio buttons
+toggleChoice: function(event)
+{
+  // get pointer to radio menu
+  var saveGroup = document.getElementById('mode');
+  if ( saveGroup == null ) // if rqadio menu not found
+  {
+    dump("\ntoggleChoice: SaveGroup not found in document");
+    return;
+  }
+  // get selected item
+  var selectedItem = saveGroup.selectedItem;
+  if ( selectedItem.id == "fb2epub-radio" ) // if selected item's id is id of our (fb2epub) menu entry
+  {
+	if ( _fb2ItemSelected == false ) // if this is first time in a row user clicked on fb2epub button (to avoid multiple clicks)
+	{
+		// record current enabled/disabled state of remember button
+		_rememberButtonDisabledPrevState = this.getRememberButtonDisabledState();
+		_rememberButtonCheckedPrevState = this.getRememberButtonCheckedState();
+		// disable the remember button
+		this.disableRememberButton(true);
+		// uncheck the button by default
+		this.checkRememberButton(false);
+		// mark that fb2epub item was selected as last select operation
+		_fb2ItemSelected = true;
+	}
+  }
+  else // if not our menu entry
+  {
+	if ( _fb2ItemSelected == true) // if prev. selection was fb2epub menu entry
+	{
+		this.disableRememberButton(_rememberButtonDisabledPrevState); // restore the enabled/disabled state that was before fb2epub menu entry was clicked first time
+		dump("\n" + _rememberButtonCheckedPrevState);
+		this.checkRememberButton(_rememberButtonCheckedPrevState); // restore the checked state that was before fb2epub menu entry was clicked first time
+		// clear fb2epub menu entry selection flag
+	    _fb2ItemSelected = false;
+	}
+  }
+},
+
 insertMenu: function (saveGroup)
 {
   var saveItem = document.getElementById('save');
   if ( saveItem == null)
   {
-    dump("\nItem with ID save not found");
+    dump("\ninsertMenu: Item with ID save not found");
   }
   else
   {
     var saveIndex = saveGroup.getIndexOfItem(saveItem);
-    var newMenuItem = null;
 	if (saveIndex  == saveGroup.itemCount ) // if 'save' - last item we append
 	{
-	  newMenuItem = saveGroup.appendItem("Fb2ePub (convert to ePub)");
+	  this._fb2MenuItem = saveGroup.appendItem("Fb2ePub (convert to ePub)");
 	}
 	else // else insert before it
 	{
-	  newMenuItem = saveGroup.insertItemAt(saveIndex+1,"Fb2ePub (convert to ePub)");
+	  this._fb2MenuItem = saveGroup.insertItemAt(saveIndex+1,"Fb2ePub (convert to ePub)");
 	}
-	if ( newMenuItem == null )
+	if ( this._fb2MenuItem == null )
 	{
-		dump("\nProblem");
+		dump("\ninsertMenu: adding menu entry failed");
 	}
 	else
 	{
-		newMenuItem.id = "fb2epub-radio";
+		this._fb2MenuItem.id = "fb2epub-radio";
+		saveGroup.addEventListener(
+      "select", function(event) {fb2SaveContent.toggleChoice(event);},true);
 	}
   }
 },
 
+// Return if rememberChoise button checked or not
+getRememberButtonCheckedState: function()
+{
+	// get the button
+	var rememberbutton = document.getElementById('rememberChoice');
+	if ( rememberbutton == null ) // if no button exit 
+	{
+	  dump("\ngetRememberButtonCheckedState: - rememberChoice button not found");
+	  return false; // not checked by default, just in case
+	}
+	var state = rememberbutton.getAttribute("checked");	
+	dump("\n" + state);
+	if ( state == true) // if checked then return true
+	{
+	  return true;
+	}
+	// if attribute set to false or not set at all
+	return false;
+},
+
+// Return if rememberChoise button disabled or not
+getRememberButtonDisabledState: function()
+{
+	// get the button
+	var rememberbutton = document.getElementById('rememberChoice');
+	if ( rememberbutton == null ) // if no button exit 
+	{
+	  dump("\ngetRememberButtonDisabledState: - rememberChoice button not found");
+	  return false; // not disabled by default, just in case
+	}
+	var state = rememberbutton.getAttribute("disabled");	
+	if ( state == true) // if disabled then return true
+	{
+	  return true;
+	}
+	// if attribute set to false or not set at all
+	return false;
+},
+
 init: function()
 {
-dump("\n");
 	if ( this.isfb2extension(dialog.mLauncher.source))
 	{
-		//dump("\nFB2 file downloading");
+		_fb2MenuItem =  null;
+		_fb2ItemSelected =  false;
+		// record initial button state
+		_rememberButtonDisabledPrevState = this.getRememberButtonDisabledState();
+		_rememberButtonCheckedPrevState = this.getRememberButtonCheckedState();
 		var saveGroup = document.getElementById('mode');
 		if ( saveGroup == null )
 		{
@@ -77,6 +190,16 @@ dump("\n");
 		else
 		{
 			this.insertMenu(saveGroup);
+			var d = document.documentElement;
+			if ( d == null )
+			{
+			  dump("\nError");
+			}
+			else
+			{
+				_original_onaccept_action = d.getAttribute('ondialogaccept'); // save original action performed on accept
+			    //d.setAttribute('ondialogaccept',"return fb2SaveContent.onConvAccept();");
+			}
 		}
 	}
 	else
@@ -85,7 +208,11 @@ dump("\n");
 	}
 },
 
-
+onConvAccept: function()
+{
+	dump("\nAccept pressed");
+	return _original_onaccept_action;
+},
 
 onload: function()
 {

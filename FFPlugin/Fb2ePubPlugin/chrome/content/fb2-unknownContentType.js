280b5b421c9d5231ca15c256e5b8050e33d3f8fb
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2014-06-09 14:16:30 +0000

    Added code that perform file download

--- FFPlugin/Fb2ePubPlugin/chrome/content/fb2-unknownContentType.js
+++ FFPlugin/Fb2ePubPlugin/chrome/content/fb2-unknownContentType.js
@@ -9,6 +9,7 @@ var fb2SaveContent = {
 	_rememberButtonDisabledPrevState: false,
 	_rememberButtonCheckedPrevState: false,
 
+	
 // Check if the passed source name is one of FB2 extensions
 isfb2extension: function(sourceURI)
 {
@@ -46,6 +47,7 @@ disableRememberButton: function(disable)
 	  dump("\ndisableRememberButton: - rememberChoice button not found");
 	  return;
 	}
+	// set disabled attribute
 	rememberbutton.setAttribute("disabled", disable);
 },
 
@@ -58,6 +60,7 @@ checkRememberButton: function(check)
 	  dump("\ncheckRememberButton: - rememberChoice button not found");
 	  return;
 	}
+	// set checked attribute
 	rememberbutton.setAttribute("checked", check);
 },
 
@@ -109,6 +112,7 @@ insertMenu: function (saveGroup)
   }
   else
   {
+  // get pointer to save item in the group
     var saveIndex = saveGroup.getIndexOfItem(saveItem);
 	if (saveIndex  == saveGroup.itemCount ) // if 'save' - last item we append
 	{
@@ -169,9 +173,94 @@ getRememberButtonDisabledState: function()
 	return false;
 },
 
+// Download a file from internet, to file named "filename" (should include path) from URI path "uri"
+download: function (filename, uri)
+{
+  try {
+    var nsILocalFile = Components.classes["@mozilla.org/file/local;1"]
+                      .getService(Components.interfaces.nsILocalFile);
+    // var nsIURI = Components.classes["@mozilla.org/network/standard-url;1"]
+                 // .getService(Components.interfaces.nsIURI);
+    nsILocalFile.initWithPath(filename);
+    // nsIURI.spec = url;
+    var nsIWBP = Components.interfaces.nsIWebBrowserPersist;
+	if ( nsIWBP == null )
+	{
+		dump("\ndownload: - Unable to create nsIWebBrowserPersist interface");
+		return null;
+	}
+    var persist = Components.classes["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"]
+                 .createInstance(nsIWBP);
+	if ( persist == null )
+	{
+		dump("\ndownload: - Unable to create nsIWebBrowserPersist interface object instance");
+		return null;
+	}
+    persist.persistFlags = nsIWBP.PERSIST_FLAGS_REPLACE_EXISTING_FILES;
+	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+	if ( wm == null )
+	{
+		dump("\ndownload: - Unable to get nsIWindowMediator");
+		return null;
+	}
+	var wrecent = wm.getMostRecentWindow("navigator:browser");
+	if ( wrecent == null )
+	{
+		dump("\ndownload: - Unable to get latest window");
+		return null;
+	}
+	Components.utils.import("resource://gre/modules/PrivateBrowsingUtils.jsm");	
+	var privacy = PrivateBrowsingUtils.privacyContextFromWindow(wrecent);
+	if ( privacy == null )
+	{
+		dump("\ndownload: - Unable to get privacy from recent window");
+		return null;
+	}
+    persist.saveURI(uri, null, null, null, null, nsILocalFile,privacy);
+    return nsILocalFile;
+  }
+  catch(ex) {
+	dump("\n"+ex);
+    return null;
+  }
+},
+
 downloadAndConvert: function()
 {
+	// get file picker dialog interface
+	var nsIFilePicker = Components.interfaces.nsIFilePicker;
+	var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+	fp.init(window, "Enter a File Name to save", nsIFilePicker.modeSave);
+	fp.appendFilter("ePub file","*.ePub");
+	fp.defaultExtension = "ePub";
+	var myURL = dialog.mLauncher.source.QueryInterface(Components.interfaces.nsIURL);
+	var extension = myURL.fileExtension.toLowerCase();
+	var basename = myURL.fileBaseName + ".ePub";
+	if (extension != "fb2") //if some kind of archive
+	{
+		var start = myURL.fileBaseName.lastIndexOf("."); // index of last .FB2 extension
+		if (start != -1) // if no more extension , then 'basename' is ok, otherwise - need to remove
+		{
+			basename = myURL.fileBaseName.substring(0,start)  + ".ePub";
+		}
+	}
+	dump("\n"+ basename);
+	fp.defaultString = basename;
+	var result = fp.show();
+	if (result == nsIFilePicker.returnOK || result == nsIFilePicker.returnReplace)
+	{
+		var savedFile = this.download(fp.file.path,dialog.mLauncher.source);
+		if ( savedFile == null )
+		{
+			dump("\ndownloadAndConvert: Unable to download file: " + fp.file.path);
+		}
 	
+		// dump("\n"+fp.file.leafName);
+		// var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+		// var wrecent = wm.getMostRecentWindow("navigator:browser");
+		// wrecent.saveURL(dialog.mLauncher.source, fp.file.leafName);
+	}
+
 },
 
 dialogAccepted: function() {
@@ -187,7 +276,7 @@ dialogAccepted: function() {
 
 init: function()
 {
-	dump("\n\nStarting:\n");
+	dump("\n\nStarting: \n" );
 	if ( this.isfb2extension(dialog.mLauncher.source))
 	{
 		this._fb2MenuItem =  null;

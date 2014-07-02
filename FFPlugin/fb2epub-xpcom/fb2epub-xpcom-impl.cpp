fe8f3c507b7c6765cb1b4691b7b6d51586b9efe8
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2014-07-02 11:44:35 +0000

    Finally basic functionality works

--- FFPlugin/fb2epub-xpcom/fb2epub-xpcom-impl.cpp
+++ FFPlugin/fb2epub-xpcom/fb2epub-xpcom-impl.cpp
@@ -1,5 +1,6 @@
 #include "fb2epub-xpcom-impl.h"
 #include "nsStringAPI.h"
+#include "..\..\Fb2EpubExt\fb2epubconverter.tlh"
 
 
 NS_IMPL_ISUPPORTS1(CFb2EpubConverterCaller, IFb2EpubConverter)
@@ -28,9 +29,32 @@ int	CFb2EpubConverterCaller::MessageBoxNS(HWND hWnd,const nsAString& nsaText,con
 }
 
 /* long Convert (in AString inputPath, in AString putputPath); */
-NS_IMETHODIMP CFb2EpubConverterCaller::Convert(const nsAString & inputPath, const nsAString & putputPath, int32_t *_retval)
+NS_IMETHODIMP CFb2EpubConverterCaller::Convert(const nsAString & inputPath, const nsAString & outputPath, int32_t *_retval)
 {
+	CoInitialize(NULL);
+	IEPubConverterInterfacePtr pConverter = NULL;
+	HRESULT hr = pConverter.CreateInstance(CLSID_ConvertProcessor);
+	if ( FAILED(hr))
+	{
+		MessageBox(NULL,L"Unable to create converter instance.\nConverter probably not registered",L"Error",MB_OK|MB_ICONERROR);
+	}
+	// convert input string to BSTR
+	char16_t* chText = ToNewUnicode(inputPath);
+	BSTR bstrInput = ::SysAllocString(chText);
+	NS_Free(chText);
+	// convert output string to BSTR
+	chText = ToNewUnicode(outputPath);
+	BSTR bstrOutput = ::SysAllocString(chText);
+	NS_Free(chText);
+	hr = pConverter->ConvertSingleFile(bstrInput,bstrOutput,NULL);
+	::SysFreeString(bstrInput);
+	::SysFreeString(bstrOutput);
+	if ( FAILED(hr))
+	{
+		MessageBox(NULL,L"Unable to convert file",L"Error",MB_OK|MB_ICONERROR);
+	}
     //return NS_ERROR_NOT_IMPLEMENTED;
-	MessageBoxNS(NULL,inputPath,putputPath,MB_OK);
+	//MessageBoxNS(NULL,inputPath,outputPath,MB_OK);
+	CoUninitialize();
 	return NS_OK;
 }

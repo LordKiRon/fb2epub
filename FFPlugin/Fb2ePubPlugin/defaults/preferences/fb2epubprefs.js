e2d5d269e0294018f598b06d81edd5af57f05b3a
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2014-07-04 19:12:21 +0000

    Added persistent preferences and localized their description

---
+++ FFPlugin/Fb2ePubPlugin/defaults/preferences/fb2epubprefs.js
@@ -0,0 +1,7 @@
+//pref("extensions.sample.sort", 2); //an int pref
+pref("extensions.fb2epub.checkforFB2ZIP", true); // filter files with FB2.ZIP extension
+pref("extensions.fb2epub.checkforFB2RAR", true); // filter files with FB2.RAR extension
+pref("extensions.fb2epub.checkforANYZIP", false); // filter files with .ZIP extension
+pref("extensions.fb2epub.checkforANYRAR", false); // filter files with .RAR extension
+pref("extensions.fb2epub.checkAdditional", false); // filter additional (user specified) extensions or not
+pref("extensions.fb2epub.additionalextensions", ""); //a string contained additional ';' separated file extensions to check/convert

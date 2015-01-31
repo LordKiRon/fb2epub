79cdf42b70e886476db4c6984ce7553ad36f9503
lordkiron@gmail.com <lordkiron@gmail.com@2cf988b9-18ac-7cdb-2130-5359ffeb1a69>
2015-01-31 21:29:49 +0000

    Added tester

---
+++ Testers/ConverterTester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FB2EPubConverter;
+
+namespace ConverterTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new ConvertProcessor();
+        }
+    }
+}
